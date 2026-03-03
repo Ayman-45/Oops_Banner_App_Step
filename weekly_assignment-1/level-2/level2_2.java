0c69adcbdea2cea3b73252ddba8e257279234bdb
Ayman-45 <ma2574@srmist.edu.in>
2026-03-03 09:00:33 +0530

    Added weekly assignment 1 and 2

---
+++ weekly_assignment-1/level-2/level2_2.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class level2_2 {
+    public static void main(String[] args){
+        float perimeter;
+        Scanner sc = new Scanner(System.in);
+        perimeter = sc.nextFloat();
+        float side = perimeter / 4;
+        System.out.println("The length of the side is "+side+" whose perimeter is "+perimeter);
+
+    }
+    
+}
