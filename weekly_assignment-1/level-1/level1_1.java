161168e7f3dcd60bf80f7e17d2bddc3537b8f2b9
Ayman-45 <ma2574@srmist.edu.in>
2026-03-03 09:12:13 +0530

    weekly assignment

--- weekly_assignment-1/level-1/level1_1.java
+++
@@ -1,7 +0,0 @@
-public class level1_1{
-    public static void main(String[] args){
-        int init_year = 2000;
-        int current_year = 2024;
-        System.out.println("Harry's age in 2024 is "+(current_year-init_year));
-    }
-}
--- weekly_assignment-1/level-1/level1_2.java
+++
@@ -1,7 +0,0 @@
-public class level1_2 {
-    public static void main(String[] args){
-        float dist_km = 10.8f;
-        float miles = 1.6f;
-        System.out.println("Ths distance 10.8 km in miles is "+(dist_km * miles));
-    }
-}
