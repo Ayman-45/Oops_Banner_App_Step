4022d36a218075f7f307af236900664a03c33119
Ayman-45 <ma2574@srmist.edu.in>
2026-03-10 09:02:50 +0530

    Added UC2

---
+++ HelloApp.java
@@ -0,0 +1,13 @@
+/**
+ * @author - Developer
+ * @version - 1.0 
+ * @since - UC1
+ * 
+ */
+
+public class HelloApp{
+    public static void main(String[] args){
+        String name = args[0];
+        System.out.println("Hello, " +name+ "!");
+    }
+}
